ef54dca0015ab2cb94145db7120a868edc94b21c
rygo0712 <ryan_jefferson_go@dlsu.edu.ph>
2024-01-23 21:14:32 +0800

    update: fixed batch file if condition

--- batch.bat
+++ batch.bat
@@ -12,19 +12,19 @@ echo ===start=== > output.txt
     echo threads equals %thr_init% >> output.txt
     set /A j=1
     :loop2
-        if "%j%" LEQ "%init_run%" @(  
+        if %j% LEQ %init_run% @(  
             echo %LIMIT%
             echo %thr_init% 
         ) | main
 
-        if "%j%" GTR "%init_run%" @(  
+        if %j% GTR %init_run% @(  
             echo %LIMIT%
             echo %thr_init% 
         ) | main >> output.txt
     set /A j=j+1
-    if "%j%" LEQ "%total_run%" goto loop2
+    if %j% LEQ %total_run% goto loop2
 
     set /A i=i+1
     set /A thr_init=thr_init*2
-if "%i%" LSS "%pow_total%" goto loop1
+if %i% LEQ %pow_total% goto loop1
 echo ===end=== >> output.txt
